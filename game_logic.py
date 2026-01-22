79180cbe517359d5b3d8095f14edf27f8c6a4736
noemie majerus <majerusnoemi@gmail.com>
2026-01-22 11:20:04 +0100

    test

--- game_logic.py
+++ game_logic.py
@@ -1,7 +1,10 @@
 ### def des fonction de jeu
 import streamlit as st
+import requests
 
+API_URL = "http://localhost:8000/move"
 
+#devinir les gagnant 
 def check_winner(b):
     wins = [
         (0, 1, 2), (3, 4, 5), (6, 7, 8),  # lignes
@@ -17,20 +20,20 @@ def play(idx):
     # si partie finie ou case déjà prise -> rien
     if st.session_state.winner or st.session_state.draw:
         return
-    if st.session_state.board[idx] != "":
+    if st.session_state.grille[idx] != "":
         return
 
     # jouer le coup
-    st.session_state.board[idx] = st.session_state.player
+    st.session_state.grille[idx] = st.session_state.player
 
     # vérifier gagnant
-    w = check_winner(st.session_state.board)
+    w = check_winner(st.session_state.grille)
     if w:
         st.session_state.winner = w
         return
 
     # vérifier match nul
-    if all(cell != "" for cell in st.session_state.board):
+    if all(cell != "" for cell in st.session_state.grille):
         st.session_state.draw = True
         return
 
@@ -39,8 +42,26 @@ def play(idx):
 
 #pour remmetre la partie a zero
 def reset():
-    st.session_state.board = [""] * 9
+    st.session_state.grille = [""] * 9
     st.session_state.player = "✖️"
     st.session_state.winner = None
     st.session_state.draw = False
 
+#apel api
+def call_ai_api(grille, player):
+    payload = {
+        "grille": grille,     # état actuel du jeu
+        "player": player   # "X" ou "O" (qui doit jouer maintenant)
+    }
+
+    response = requests.post(API_URL, json=payload, timeout=10)
+    response.raise_for_status() #= si sa marche pas arrete toi la
+
+    data = response.json()
+
+    # si l'API renvoie directement la liste
+   # if isinstance(data, list):
+       # return data
+
+    # si l'API renvoie un JSON
+    return data["grille"]
